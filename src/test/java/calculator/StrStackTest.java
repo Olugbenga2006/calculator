82552cc1a81291b1e0f9b9648866b725bc7d13ea
Olugbenga2006 <157271915+Olugbenga2006@users.noreply.github.com>
2024-08-06 02:28:05 +0100

    new branch to add three new classes, numstack,opstack and strstack

---
+++ src/test/java/calculator/StrStackTest.java
@@ -0,0 +1,13 @@
+package calculator;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+class StrStackTest {
+
+  @Test
+  void test() {
+    fail("Not yet implemented");
+  }
+
+}
