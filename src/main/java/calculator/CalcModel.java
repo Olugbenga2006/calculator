c08967db4b0bd30cc667a034c78f1e186acbc487
Olugbenga2006 <157271915+Olugbenga2006@users.noreply.github.com>
2024-08-12 16:13:18 +0100

    last commit was faulty.contents of calccontroller,calcview,calcmodel,driver and OpType were empty. this will add all the contents

--- src/main/java/calculator/CalcModel.java
+++ src/main/java/calculator/CalcModel.java
@@ -1,5 +1,13 @@
 package calculator;
 
-public class CalcModel {
+import calculator.Calculator;
 
+/**
+ * Evaluates an expression - the evaluation can be Standard (infix) or reverse polish.
+ */
+public class CalcModel implements Calculator {
+  @Override
+  public float evaluate(String expression, Boolean infix) {
+    return 0.0f;
+  }
 }
